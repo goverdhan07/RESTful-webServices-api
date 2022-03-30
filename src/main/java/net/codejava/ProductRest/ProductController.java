c1bb258b769b92bbc6412773de6741f32d725e7e
goverdhan07 <66107302+goverdhan07@users.noreply.github.com>
2022-03-30 07:13:16 +0530

    fixed connecting to server related issue

--- src/main/java/net/codejava/ProductRest/ProductController.java
+++ src/main/java/net/codejava/ProductRest/ProductController.java
@@ -1,12 +1,13 @@
 package net.codejava.ProductRest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller //A controller basically controls the flow of the data.
+@RestController //A controller basically controls the flow of the data.
 public class ProductController {
     @Autowired
     public ProductService service;
@@ -15,16 +16,15 @@ public class ProductController {
     public List<Product> list() {
         return service.listAll();
     }
-}
 
-
-//    public void save(Product product){
+//    public void save(Product product) {
 //        service.save(product);
 //    }
-//
+}
+
 //    @GetMapping("/products/{id}")
-//    public Product get(Integer id){
-//        return service.findById(id).get();
+//    public Product get(@PathVariable Integer id){
+//        return service.get(id);
 //    }
 //
 //    public void delete(Integer id){
